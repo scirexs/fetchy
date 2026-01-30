cfb1cf72a2e630b15c6dae1abcdd71c04aa1467d
scirexs <git@scirexs.dev>
2026-01-30 09:29:51 +0000

    fix: correct export methods

--- src/mod.ts
+++ src/mod.ts
@@ -3,5 +3,5 @@
  * @module
  */
 
-export { Fetchy, fetchy, fy, HTTPStatusError, sfetchy } from "./main.ts";
+export { fetchy, fy, HTTPStatusError, setFetchy, sfetchy } from "./main.ts";
 export type { FetchyBody, FetchyOptions, FetchyResponse, FetchySafeResponse, JSONValue, RetryOptions } from "./types.ts";
